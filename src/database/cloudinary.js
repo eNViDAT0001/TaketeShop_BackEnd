f3ceffe5eaf303f2695ceebbb8a5144e1e503be2
eNViDAT-student <nvdat.ngvan@gmail.com>
2022-05-05 08:10:03 +0700

    Add Cloudinary

---
+++ src/database/cloudinary.js
@@ -0,0 +1,6 @@
+const cloudinary = require('cloudinary')
+cloudinary.config({ 
+    cloud_name: 'taketeshop', 
+    api_key: '862947269136143', 
+    api_secret: 'cDmGtX3-UrfdPaP0ro_t3UMUVcs' 
+  });

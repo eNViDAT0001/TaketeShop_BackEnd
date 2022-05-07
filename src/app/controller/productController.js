5325d002ea08090247af744d7af87dadb27a1e30
eNViDAT-student <nvdat.ngvan@gmail.com>
2022-05-07 23:40:00 +0700

    getProduct

--- src/app/controller/productController.js
+++ src/app/controller/productController.js
@@ -11,48 +11,41 @@ class ProductController {
     try {
       var command =
         "SELECT " +
-        "Product.id" +
-        ", " +
-        "Product.category_id" +
-        ", " +
-        "Product.user_id" +
-        ", " +
-        "Product.unit_id" +
-        ", " +
-        "Product.name" +
-        ", " +
-        "Product.descriptions" +
-        ", " +
-        "Product.price" +
-        ", " +
-        "Product.quantity" +
-        ", " +
-        "Unit.name AS unit" +
-        ", " +
-        "Product.discount" +
-        ", " +
-        "CONVERT(IF (SUM(OrderItems.quantity) IS null, 0, SUM(OrderItems.quantity)), UNSIGNED) AS sold" +
-        ", " +
-        "GROUP_CONCAT(CONCAT(\"{id: \",ProductImage.id,\"\"), CONCAT(\", image: '\",ProductImage.image_path,\"'}\")) as image" +
-        ", " +
-        "Product.create_time" +
-        ", " +
-        "Product.update_time " +
+        "result.*, " +
+        "CONVERT(IF (SUM(OrderItems.quantity) IS null, 0, SUM(OrderItems.quantity)), UNSIGNED) AS sold " +
+        "FROM ( " +
+        "SELECT " +
+        "Product.create_time, " +
+        "Product.update_time, " +
+        "Product.id, " +
+        "Product.category_id, " +
+        "Product.user_id, " +
+        "Product.unit_id, " +
+        "Product.name, " +
+        "Category.name as category_name, " +
+        "Product.descriptions, " +
+        "Product.price, " +
+        "Product.quantity, " +
+        "Product.discount, " +
+        "Unit.name as unit, " +
+        'GROUP_CONCAT(CONCAT("{id: ",ProductImage.id,""), CONCAT(", image: \'",ProductImage.image_path,"\'}")) as images ' +
         "FROM Product " +
-        "INNER JOIN Unit ON Unit.id = Product.unit_id " +
-        "LEFT JOIN OrderItems ON OrderItems.product_id = Product.id " +
-        "LEFT JOIN ProductImage ON ProductImage.product_id = Product.id " +
-        "GROUP BY Product.id";
+        "JOIN Category ON Product.category_id = Category.id " +
+        "JOIN ProductImage ON ProductImage.product_id = Product.id " +
+        "JOIN Unit ON Product.unit_id = Unit.id GROUP by ProductImage.product_id) result " +
+        "LEFT JOIN OrderItems ON OrderItems.product_id = result.id " +
+        "GROUP by result.id " +
+        "ORDER BY result.id asc;";
       SQLpool.execute(command, (err, result, field) => {
         if (err) throw err;
         res.send(result);
       });
     } catch (err) {
       console.log(err);
-        res.send({
-          error: true,
-          msg: err,
-        });
+      res.send({
+        error: true,
+        msg: err,
+      });
     }
   }
 
@@ -67,10 +60,10 @@ class ProductController {
       });
     } catch (err) {
       console.log(err);
-        res.send({
-          error: true,
-          msg: err,
-        });
+      res.send({
+        error: true,
+        msg: err,
+      });
     }
   }
   async getProductByID(req, res) {
@@ -83,10 +76,10 @@ class ProductController {
       });
     } catch (err) {
       console.log(err);
-        res.send({
-          error: true,
-          msg: err,
-        });
+      res.send({
+        error: true,
+        msg: err,
+      });
     }
   }
 
@@ -110,16 +103,25 @@ class ProductController {
       });
     } catch (err) {
       console.log(err);
-        res.send({
-          error: true,
-          msg: err,
-        });
+      res.send({
+        error: true,
+        msg: err,
+      });
     }
   }
 
   async addProductWithoutImage(req, res, next) {
     try {
-      var {categoryID, userID, name, descriptions, price, quantity, discount, unitID} = req.body;
+      var {
+        categoryID,
+        userID,
+        name,
+        descriptions,
+        price,
+        quantity,
+        discount,
+        unitID,
+      } = req.body;
       var command =
         "INSERT INTO `Product` (`id`, `category_id`, `user_id`, `name`, `descriptions`, `price`, `quantity`, `unit_id`, `discount`, `create_time`, `update_time`) VALUES (NULL, '" +
         categoryID +
@@ -146,10 +148,10 @@ class ProductController {
       });
     } catch (err) {
       console.log(err);
-        res.send({
-          error: true,
-          msg: err,
-        });
+      res.send({
+        error: true,
+        msg: err,
+      });
     }
   }
 }
